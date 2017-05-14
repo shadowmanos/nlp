45dbfde94a93068971e6ca7af8292846ca8195b8
shadowmanos <shadowmanos@users.noreply.github.com>
2017-05-14 21:31:47 +0100

    first example and readme

---
+++ corenlp/src/main/java/intro/SimpleExample.java
@@ -0,0 +1,41 @@
+package intro;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+class SimpleExample {
+    private static final StanfordCoreNLP pipeline;
+
+    static {
+        final Properties props = new Properties();
+
+        // tokenize: split the text in tokens (words and punctuation)
+        // ssplit: group the above tokens in sentences according to punctuation
+        // pos: tags words as nouns, verbs etc
+        props.put("annotators", "tokenize ssplit pos");
+
+        // initialize the processing engine for the above annotators
+        pipeline = new StanfordCoreNLP(props);
+    }
+
+    public static void main(String[] args) throws IOException {
+        // an appropriate sample text for starting our journey
+        final String text = "Space: the final frontier. " +
+            "These are the voyages of the starship Enterprise. " +
+            "Its continuing mission: to explore strange new worlds, " +
+            "to seek out new life and new civilizations, " +
+            "to boldly go where no one has gone before.";
+
+        // the Annotation instance, containing both input and output of the processing
+        final Annotation document = new Annotation(text);
+
+        // process the text and store the result in Annotation instance
+        pipeline.annotate(document);
+
+        // output the annotation at stdout as a JSON
+        pipeline.jsonPrint(document, new PrintWriter(System.out));
+    }
+}
---
+++ docs/Readme.md
@@ -0,0 +1,19 @@
+#### NLP cookbook for Java developers
+
+I have gathered here some practical examples of [Natural Language
+Processing](https://en.wikipedia.org/wiki/Natural_language_processing),
+using Java libraries like
+[Stanford CoreNLP](https://stanfordnlp.github.io/CoreNLP/).
+
+The code and any accompanying documentation is written from a developer's
+rather than a PhD student's perspective.
+That reflects my personal experience from when I started doing NLP at work
+and I could find plenty of scientific papers but few code snippets.
+
+All text samples used for testing are Star Trek quotes
+(English not Klingon). After an entirely unscientific thought process,
+I decided the quotes are grammatically correct, adequately diverse and
+of average complexity.
+
+*Disclaimer*: No [tribbles](https://en.wikipedia.org/wiki/Tribble)
+were harmed in the making of this repository.
