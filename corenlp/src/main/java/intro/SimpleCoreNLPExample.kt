1fb5c3299b996526d613a5c087260904faba4a31
shadowmanos <shadowmanos@users.noreply.github.com>
2019-03-01 12:06:20 +0000

    kotlin

--- corenlp/src/main/java/intro/SimpleCoreNLPExample.kt
+++ corenlp/src/main/java/intro/SimpleCoreNLPExample.kt
@@ -3,34 +3,32 @@ package intro
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation
 import edu.stanford.nlp.pipeline.JSONOutputter
 import edu.stanford.nlp.pipeline.StanfordCoreNLP
-import java.io.IOException
+import org.apache.commons.lang3.StringUtils.isBlank
 import java.util.Properties
 
-object SimpleCoreNLPExample {
-    private val pipeline: StanfordCoreNLP
+fun main() {
+    val props = Properties()
 
-    init {
-        val props = Properties()
+    // tokenize: split the text in tokens (words and punctuation)
+    // ssplit: group the above tokens in sentences according to punctuation
+    // pos: tags words as nouns, verbs etc
+    props["annotators"] = "tokenize ssplit pos"
 
-        // tokenize: split the text in tokens (words and punctuation)
-        // ssplit: group the above tokens in sentences according to punctuation
-        // pos: tags words as nouns, verbs etc
-        props["annotators"] = "tokenize ssplit pos"
+    // initialize the processing engine for the above annotators
+    val pipeline = StanfordCoreNLP(props)
 
-        // initialize the processing engine for the above annotators
-        pipeline = StanfordCoreNLP(props)
-    }
+    // appropriate sample text for starting our journey
+//    val text = "Space: the final frontier. " +
+//            "These are the voyages of the starship Enterprise. " +
+//            "Its continuing mission: to explore strange new worlds, " +
+//            "to seek out new life and new civilizations, " +
+//            "to boldly go where no one has gone before."
+    // cue pompous theme music
 
-    @Throws(IOException::class)
-    @JvmStatic
-    fun main() {
-        // appropriate sample text for starting our journey
-        val text = "Space: the final frontier. " +
-                "These are the voyages of the starship Enterprise. " +
-                "Its continuing mission: to explore strange new worlds, " +
-                "to seek out new life and new civilizations, " +
-                "to boldly go where no one has gone before."
-        // cue pompous theme music
+    while (true) {
+        val text = readLine()
+
+        if (isBlank(text)) return
 
         // the Annotation instance, containing both input and output of the processing
         val annotation = pipeline.process(text)
